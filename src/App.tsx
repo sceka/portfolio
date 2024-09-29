a5b36e310745366befe0e1eb43221e7f86bc5c75
Marko Scekic <scekicm02@gmail.com>
2024-09-29 14:06:20 +0200

    Add fonts and establish variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.scss";
-import Navbar from "./components/Navbar/Navbar";
+import "./styles/fonts.scss";
 
+import Navbar from "./components/Navbar/Navbar";
 function App() {
 	return (
 		<div className='App'>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,5 +2,16 @@ import React from "react";
 import "./Navbar.scss";
 
 export default function Navbar() {
-	return <div className='n-container'>Navbar</div>;
+	return (
+		<div className='n-container'>
+			<div>Marko Scekic</div>
+			<div className='navbar-menu'>
+				<p>About</p>
+				<p>Projects</p>
+				<p>Bla</p>
+				<p>Truc</p>
+			</div>
+			<div>En Russian</div>
+		</div>
+	);
 }
