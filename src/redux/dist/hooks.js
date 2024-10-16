54219a1b10b73f1740db90ed66a644e520172f30
Marko Scekic <scekicm02@gmail.com>
2024-10-17 00:49:28 +0200

    Remove redux dist

--- src/redux/dist/hooks.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.useAppStore = exports.useAppSelector = exports.useAppDispatch = void 0;
-var react_redux_1 = require("react-redux");
-exports.useAppDispatch = react_redux_1.useDispatch.withTypes();
-exports.useAppSelector = react_redux_1.useSelector.withTypes();
-exports.useAppStore = react_redux_1.useStore.withTypes();
