ab2d9feeeb698a13c8b4d77a84c6db023b23d08b
Marko Scekic <scekicm02@gmail.com>
2024-10-17 15:14:37 +0200

    ADd work experience

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -2,11 +2,51 @@ import React from "react";
 import "./Work.scss";
 
 export default function Work() {
+	const workExperience = [
+		{
+			name: "Fuel Me",
+			years: "Aug 2023 - Septembar 2024",
+			total: "1 year 1 month",
+			position: "Senior Backend Developer",
+			type: "Hybrid"
+		},
+		{
+			name: "Fuel Me",
+			years: "March 2023 - August 2023",
+			total: "1 year 1 month",
+			position: "Full Stack Developer",
+			type: "Remote"
+		},
+		{
+			name: "Wiv Technology",
+			years: "Decembar 2021 - March 2023",
+			total: "1 year 4 months",
+			position: "Full Stack Developer",
+			type: "Remote"
+		},
+		{
+			name: "Fuel Me",
+			years: "Jun 2021 - November 2021",
+			total: "6 months",
+			position: "Full Stack Developer",
+			type: "Remote"
+		}
+	];
 	return (
 		<div className='work-container'>
-			<div className='technologies'>
-				<h1>FuelMe</h1>
-			</div>
+			<h1>Work</h1>
+			{workExperience.map(work => (
+				<div className='work-experience'>
+					<div className='years'>
+						<p className='year'>{work.years}</p>
+						<p className='total-years'>{work.total}</p>
+					</div>
+					<div className='name'>{work.name}</div>
+					<div className='position'>
+						{work.position} | {work.type}
+					</div>
+				</div>
+			))}
 		</div>
 	);
 }
---
+++ src/components/Work/dist/Work.js
@@ -0,0 +1,48 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+require("./Work.scss");
+function Work() {
+    var workExperience = [
+        {
+            name: "Fuel Me",
+            years: "Aug 2023 - Septembar 2024",
+            total: "1 year 1 month",
+            position: "Senior Backend Developer",
+            type: "Hybrid"
+        },
+        {
+            name: "Fuel Me",
+            years: "March 2023 - August 2023",
+            total: "1 year 1 month",
+            position: "Full Stack Developer",
+            type: "Remote"
+        },
+        {
+            name: "Wiv Technology",
+            years: "Decembar 2021 - March 2023",
+            total: "1 year 4 months",
+            position: "Full Stack Developer",
+            type: "Remote"
+        },
+        {
+            name: "Fuel Me",
+            years: "Jun 2021 - November 2021",
+            total: "6 months",
+            position: "Full Stack Developer",
+            type: "Remote"
+        }
+    ];
+    return (react_1["default"].createElement("div", { className: 'work-container' },
+        react_1["default"].createElement("h1", null, "Work"),
+        workExperience.map(function (work) { return (react_1["default"].createElement("div", { className: 'work-experience' },
+            react_1["default"].createElement("div", { className: 'years' },
+                react_1["default"].createElement("p", { className: 'year' }, work.years),
+                react_1["default"].createElement("p", { className: 'total-years' }, work.total)),
+            react_1["default"].createElement("div", { className: 'name' }, work.name),
+            react_1["default"].createElement("div", { className: 'position' },
+                work.position,
+                " | ",
+                work.type))); })));
+}
+exports["default"] = Work;
